113136635c73bec018039faf95091525ceb93f94
Ayush Kumar Sinha <ayush.sinha@housing.com>
2023-10-19 11:05:30 +0530

    Initial Commit

---
+++ MovieDB/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MovieDB
+//
+//  Created by Ayush Kumar Sinha on 19/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
